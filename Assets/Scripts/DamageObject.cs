6136f016f0ff22bd13b6e7efd8041eaeda5150da
Juan Cruz Risso <juancrissorolando@gmail.com>
2021-05-04 21:11:35 -0300

    Plataformas2D

---
+++ Assets/Scripts/DamageObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DamageObject : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Player Damage");
+            SceneManager.LoadScene("Nivel");
+            
+        }
+    }
+
+}
---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Enemy : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Player"))
+        {
+            Debug.Log("Player Damage");
+            SceneManager.LoadScene("Nivel");
+        }
+    }
+
+
+}
